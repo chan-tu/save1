c9d9faa3ab311ab6e375fe8bfecf899fb49bbfe5
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-03-28 14:56:46 +0900

    Create MainActivity.kt

---
+++ 안드로이드 앱 개발 입문 1주차/이벤트 버튼1/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.practice2
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /*fun onButton1Clicked(v: View) {
+        Toast.makeText(this,"시작!", Toast.LENGTH_LONG).show()
+    }*/
