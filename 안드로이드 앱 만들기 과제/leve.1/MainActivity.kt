e9335b975ac2ea39cf9f4d4c9e50757e617d1594
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-03-29 11:30:24 +0900

    Update MainActivity.kt

--- 안드로이드 앱 만들기 과제/leve.1/MainActivity.kt
+++ 안드로이드 앱 만들기 과제/leve.1/MainActivity.kt
@@ -1,4 +1,5 @@
-package com.example.practice3
+package com.example.calculater_application
+
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,27 +8,33 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    lateinit var login: Button
-    lateinit var Text_ID: EditText
-    lateinit var Text_Password: EditText
-    lateinit var join: Button
+    private lateinit var login: Button
+    private lateinit var iD: EditText
+    private lateinit var password: EditText
+    private lateinit var join: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        login.setOnClickListener {
-            var use_id = Text_ID.text.toString()
-            var use_pass = Text_Password.text.toString()
 
-            if (use_id == Text_ID.text.toString() || use_pass == Text_Password.text.toString()) {
+        login=findViewById(R.id.loginButton)
+        join=findViewById(R.id.joinButton)
+        iD=findViewById(R.id.textID)
+        password=findViewById(R.id.textPassword)
+
+        login.setOnClickListener {
+            val useId = iD.text.toString()
+            val usePass = password.text.toString()
+
+            if (useId == iD.text.toString() && usePass == password.text.toString()) {
                 Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
-            } else if (use_id == Text_ID.text.toString() || use_pass == "") {
+            } else if (useId == iD.text.toString() && usePass == "") {
                 Toast.makeText(this, "비밀번호를 입력하시오", Toast.LENGTH_SHORT).show()
-            } else if (use_id == "" || use_pass == Text_Password.text.toString()) {
+            } else if (useId == "" && usePass == password.text.toString()) {
                 Toast.makeText(this, "아이디를 입력하시오", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, "아이디와 비밀번호를 모두 입력하시오.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "아이디와 비밀번호을 모두 입력하시오", Toast.LENGTH_SHORT).show()
             }
 
         }
