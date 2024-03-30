9c409998011cf5aa752a6ad1ffdf2e995b86bc1f
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-03-31 07:29:43 +0900

    Create MainActivity.kt

---
+++ Kotlin문법 종합반2주차/변수와 상수/문자열의 나타내는 변수/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.practice
+
+fun main(){
+// var name:String = "참새"
+    var name = "참새"
+    println("새의 이름을 저장하는 name 변수에는 ${name}가 들어있어")
+
+    name = "조롱박이" // name변수에 들어있던 "참새" 대신에 "조롱박이"를 대입
+    println("변경한 변수 name에는 ${name}가 들어있어")
+}
