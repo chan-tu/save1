bea61c8e9e61619054fb2920196267ab4971e3a4
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-03-31 07:08:30 +0900

    Create MainActivity.kt

---
+++ Kotlin문법 종합반2주차/변수와 상수/숫자을 나타내는 숫자/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.practice
+
+fun main(){
+// var num1:Int = 1
+// var 변수이름:자료형(생략가능) = 값
+// 값을 특정 자료형을 저장할 수 있는 변수이름에 대입한다
+    var num1 = 1 // 변수 이름 'var'에 자료형'num1'를 선언하여 정수 1를 저장
+    println("내 첫변수 num1 변수에는 ${num1}이 들어있어") // 'nun1'에 저장된 값 출력
+
+    num1 = 5 //자료형'num1'를 선언하여 정수 5로 변경하여 저장
+    println("변경한 변수 num1 변수에는 ${num1}이 들어있어") //'num1'에 변경되어 저장된 값 출력
+}
