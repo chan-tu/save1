487b7b28cf73a382219da250d9cbc14917762558
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-03-31 05:20:02 +0900

    Update MainActivity.kt

--- Kotlin문법 종합반2주차/출력과 입력/키보에서 입력한 내용 출력(텍스트)/MainActivity.kt
+++ Kotlin문법 종합반2주차/출력과 입력/키보에서 입력한 내용 출력(텍스트)/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.practice
 
 fun main(){
-    val dataString = readLine() //키보에서 입력한 내용을 'dataString'에 담아 'println'으로 출력
-    println("나는 ${dataString }를 입력했어요!")
+    val dataString = readLine() //'readLine'에서 입력한 'dataString'에 저장 
+    println("나는 ${dataString }를 입력했어요!")//'println'으로 출력
 }
