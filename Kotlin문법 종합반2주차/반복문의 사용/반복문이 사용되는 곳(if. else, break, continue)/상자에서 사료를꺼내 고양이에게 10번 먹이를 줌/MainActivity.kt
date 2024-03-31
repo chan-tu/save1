db150531ba1f728b661a7267a2ef2791444ff713
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-04-01 03:12:36 +0900

    Create MainActivity.kt

---
+++ Kotlin문법 종합반2주차/반복문의 사용/반복문이 사용되는 곳(if. else, break, continue)/상자에서 사료를꺼내 고양이에게 10번 먹이를 줌/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.practice
+
+fun main(){
+    var catStatus = false
+    println("고양이는 매우 배고픕니다..")
+    println("사료를 10번 먹여주세요")
+
+
+    // 고양이가 현재 배고프니까(false) 사료 배급가능
+    if(catStatus == false) {
+        for(idx in 0..9) {
+            ///0~9까지 반복하겠다는 의미
+            println("${idx}번째: 냠냠")
+        }
+    }
+
+    // 사료를 10번 준 이후의 상태변화
+    catStatus = true
+    if(catStatus == true) {
+        println("배부른 고양이입니다")
+    }
+}
