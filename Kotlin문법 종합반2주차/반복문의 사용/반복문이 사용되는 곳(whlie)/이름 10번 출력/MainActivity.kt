d645764426c907de5997d5d6081ff76b4061aff3
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-03-31 19:07:24 +0900

    Update MainActivity.kt

--- Kotlin문법 종합반2주차/반복문의 사용/반복문이 사용되는 곳(whlie)/이름 10번 출력/MainActivity.kt
+++ Kotlin문법 종합반2주차/반복문의 사용/반복문이 사용되는 곳(whlie)/이름 10번 출력/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.practice
 
 fun main(){
-    var i = 0
+    var i = readln().toInt()
 
-    while(i < 10) {
+    while(i < 10) { // i가 10보다 클때까지 계속 실행
         println("${i}번째: 내 이름은 참새")
-        i++
+        i++//for 같은 경우에는 i를 상수로 자동설정했기 때문에 1증가가 안됬지만  while는 변수를 따로 만들어서 사용하고 있기 때문에 1 증가가 가능
     }
 }
