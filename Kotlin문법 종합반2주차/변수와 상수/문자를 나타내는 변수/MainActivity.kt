8d546fc048e91e1599b6489c41b6babcb7257f5b
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-03-31 09:43:16 +0900

    Update MainActivity.kt

--- Kotlin문법 종합반2주차/변수와 상수/문자를 나타내는 변수/MainActivity.kt
+++ Kotlin문법 종합반2주차/변수와 상수/문자를 나타내는 변수/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.practice
 fun main(){
 // var one1:Char = '김'
 // var 변수이름:타입(생략가능) = 값
-    var one1 = '김' //변수 'var'에 변수 'one1'를 선언하여 문자 '김'를 저장
+    var one1 = '김' //변수 'var'에 변수이름 'one1'를 선언하여 문자 '김'를 저장
     println("내 첫변수 one1 변수에는 ${one1}이 들어있어")//'one1'에 저장된 값 출력
 
     one1 = '나' // one1변수에 들어있던 '김' 대신에 '나'를 대입
