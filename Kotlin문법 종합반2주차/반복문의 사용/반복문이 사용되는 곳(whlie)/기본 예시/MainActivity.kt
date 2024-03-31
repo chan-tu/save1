2f2152170e91faffb0b850611e8084d584e745b4
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-04-01 02:41:56 +0900

    Create MainActivity.kt

---
+++ Kotlin문법 종합반2주차/반복문의 사용/반복문이 사용되는 곳(whlie)/기본 예시/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.practice
+
+fun main(){
+    // while 문 예시
+    println("\nwhile 문 예시:")
+    var j = 0
+    while (j < 5) {
+        println("현재 값은 $j 입니다.")
+        j++
+    }
+}
