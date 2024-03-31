ff4e8f18891def39fd770f7886321a9b6c004e3f
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-03-31 10:00:15 +0900

    Create MaicnActivity.kt

---
+++ Kotlin문법 종합반2주차/변수와 상수/변수/숫자를 나타내는 변수/MaicnActivity.kt
@@ -0,0 +1,12 @@
+package com.example.practice
+
+fun main(){
+// var num1:Int = 1
+// var 변수이름:자료형(생략가능) = 값
+// 값을 특정 자료형을 저장할 수 있는 변수이름에 대입한다
+    var num1 = 1 // 변수 var에 변수 이름'num1'를 선언하여 정수 1를 저장
+    println("내 첫변수 num1 변수에는 ${num1}이 들어있어") // 'nun1'에 저장된 값 출력
+
+    num1 = 5 //one1변수에 들어있던 1 대신에 5 를 대입
+    println("변경한 변수 num1 변수에는 ${num1}이 들어있어") //'num1'에 바뀐 값 출력
+}
