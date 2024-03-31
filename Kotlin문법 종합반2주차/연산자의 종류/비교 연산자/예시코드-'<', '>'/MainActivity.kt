b70260df0e1e07b3430ec705ae79da2367d48fb1
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-03-31 13:07:32 +0900

    Update MainActivity.kt

--- Kotlin문법 종합반2주차/연산자의 종류/비교 연산자/예시코드-'<', '>'/MainActivity.kt
+++ Kotlin문법 종합반2주차/연산자의 종류/비교 연산자/예시코드-'<', '>'/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.practice
 
 fun main(){
-    // 수학은 94점
-    var mathScore = 94
+    // 수학점수 입력
+    var mathScore = readln().toInt()
+    println("내 수학점수 ${mathScore}")
 
     // 수학점수가 90점 초과인지 판단
-    var isMathHighRank = mathScore > 90
+    var isMathHighRank = mathScore > 90 //연산자 '>'를 사용
 
     // 90점 초과라면 true (참)
     // 90점 이하라면 false (거짓)
