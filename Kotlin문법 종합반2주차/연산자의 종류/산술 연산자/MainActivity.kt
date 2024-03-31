09d7801ffb1681ef105c63afe4ce1fb236fc3907
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-03-31 10:12:10 +0900

    Create MainActivity.kt

---
+++ Kotlin문법 종합반2주차/연산자의 종류/산술 연산자/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.practice
+
+fun main(){
+    var num1 = 30 //num1에 정수 30을 저장
+    var num2 = 10 //num2에 정수 10을 저장
+
+    // 덧셈 연산자(+)사용해서 num1과 num2를 더한후에 plusResult에 대입함
+    var plusResult = num1 + num2 //30+10과 같은 의미
+    println("num1 + num2는 ${plusResult}이야")
+
+    // 뺄셈 연산자(-)사용해서 num1에서 num2를 뺀후에 minusResult에 대입함
+    var minusResult = num1 - num2
+    println("num1 - num2는 ${minusResult}이야")
+
+    // 곱셈 연산자(*)사용해서 num1과 num2를 곱한후에 multipleResult에 대입함
+    var multipleResult = num1 * num2
+    println("num1 * num2는 ${multipleResult}이야")
+
+    // 나눗셈 연산자(/)사용해서 num1에서 num2를 나눈후에 divideResult에 대입함
+    var divideResult = num1 / num2
+    println("num1 / num2는 ${divideResult}이야")
+
+    // num2를 10에서 7로 변경
+    num2 = 7
+
+    // 나머지 연산자(%)사용해서 num1에서 num2를 나눈 나머지를 modResult에 대입함
+    var modResult = num1 % num2
+    println("num1 % num2는 정수로 ${modResult}야")
+}
