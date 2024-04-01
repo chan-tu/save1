5154025e9385a1f7a5805f25a0e80dcd03b5b55c
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-04-02 05:28:55 +0900

    Update MembershipActivity.kt

--- 기본 앱/Main/MembershipActivity.kt
+++ 기본 앱/Main/MembershipActivity.kt
@@ -21,7 +21,7 @@ class MembershipActivity : AppCompatActivity() {
             if(nameEt.text.toString().trim().isEmpty()||idEt.text.toString().trim().isEmpty()||pwEt.text.toString().trim().isEmpty()){
                 Toast.makeText(this,getString(R.string.No_Information),Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
-            }
+            }//예외처리, 이름, 아이디, 비밀번호 둘중하나만 비어 있으면 메시지 출력
 
             val intent=Intent(this,LoginActivity::class.java).apply {
                 putExtra("ID",idEt.text.toString())
@@ -29,8 +29,11 @@ class MembershipActivity : AppCompatActivity() {
             }
 
             setResult(RESULT_OK,intent)
+            //RESULT_OK:-1로 정의
+            //setResult를하면 LoginActivity에서 답장 받음
 
-            if (!isFinishing)finish()
+            if(!isFinishing)finish()
+            //isFinishing: 앱또는 화면이 종료 중인지 확인
         }
     }
 }
