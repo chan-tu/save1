4c8d24448596f2237be50318541423fd0b1e03ef
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-04-02 05:21:03 +0900

    Create MembershipActivity.kt

---
+++ 기본 앱/Main/MembershipActivity.kt
@@ -0,0 +1,36 @@
+package com.example.app
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MembershipActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_membership)
+
+        val nameEt=findViewById<EditText>(R.id.name_Et)
+        val idEt=findViewById<EditText>(R.id.id_Et)
+        val pwEt=findViewById<EditText>(R.id.pass_Et)
+        val joinBt=findViewById<Button>(R.id.btn_join)
+
+        joinBt.setOnClickListener {
+            if(nameEt.text.toString().trim().isEmpty()||idEt.text.toString().trim().isEmpty()||pwEt.text.toString().trim().isEmpty()){
+                Toast.makeText(this,getString(R.string.No_Information),Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val intent=Intent(this,LoginActivity::class.java).apply {
+                putExtra("ID",idEt.text.toString())
+                putExtra("PW",pwEt.text.toString())
+            }
+
+            setResult(RESULT_OK,intent)
+
+            if (!isFinishing)finish()
+        }
+    }
+}
