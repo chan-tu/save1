6f14f352e0b85de3b47535262fa6f9be9e30ce6d
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-04-02 05:27:18 +0900

    Update LoginActivity.kt

--- 기본 앱/Main/LoginActivity.kt
+++ 기본 앱/Main/LoginActivity.kt
@@ -33,18 +33,29 @@ class LoginActivity : AppCompatActivity() {
             if (etId.text.trim().isEmpty()||etPw.text.trim().isEmpty()){
                 Toast.makeText(this,getString(R.string.toast_message),Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
+                //trim(): 공간빼기
+                //isEmpty():비어있는지 확인
+                //getString으로 텍스트 문구 호출
+                //여기서 함수를 종료해야하기 때문에 리턴 값 호출
             }
+            
             val intent=Intent(this,MainhomeActivity::class.java)
             intent.putExtra("ID",etId.text.toString())
             startActivity(intent)
-        }
+            //putExtra("키값", 호출 내용)
+            //Loging 눌렀을 떄 HomeActivity 실행
+            
+        }//호출하면 끝
 
         btJi.setOnClickListener {
             val intent=Intent(this,MembershipActivity::class.java)
             startActivity(intent)
 
-            /*val intent=Intent(this,MainhomeActivity::class.java)
-            activityResultLauncher.launch(intent)*/
-        }
+            val intent=Intent(this,MainhomeActivity::class.java)
+            activityResultLauncher.launch(intent)
+            
+            //MembershipActivity를 답장을 줘라고 하면서 호출
+            //답장이 오면서 activityResultLauncher 실행
+        }//답장을 다시 보내라는 요청을 했을 떄 activityResultLauncher가 실행
     }
 }
