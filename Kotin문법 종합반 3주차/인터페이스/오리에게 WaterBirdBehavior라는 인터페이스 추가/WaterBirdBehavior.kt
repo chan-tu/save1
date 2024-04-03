7c07499a5990033d136086395314e00a27b77cbf
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-04-03 12:44:50 +0900

    Create WaterBirdBehavior.kt

---
+++ Kotin문법 종합반 3주차/인터페이스/오리에게 WaterBirdBehavior라는 인터페이스 추가/WaterBirdBehavior.kt
@@ -0,0 +1,10 @@
+package com.example.app
+
+interface WaterBirdBehavior {
+    fun swim()
+    //WaterBirdBehaviorsms swim(헤엄치다)라는 행위 가지고 있음
+    //추상 메소드에 내용이 없는 것은 이 메소드에 해당하는 클래스에서 받아 오버라이딩 할 것이기 떄문
+//    fun swim() {
+//        println("수영 합니다")
+//    }
+}
