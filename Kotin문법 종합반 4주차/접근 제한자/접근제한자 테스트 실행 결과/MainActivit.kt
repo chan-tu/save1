ee1eb5fb6196d975d8f01b49a3492d0188673c9e
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-04-03 14:02:09 +0900

    Create MainActivit.kt

---
+++ Kotin문법 종합반 4주차/접근 제한자/접근제한자 테스트 실행 결과/MainActivit.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var accessTestClass = AccessTestClass()
+    var accessTestChildClass = AccessTestChildClass()
+
+// . 하고 접근가능한 요소를 확인
+//    accessTestClass. 
+    accessTestChildClass.protectedTest1()
+}
