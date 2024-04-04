851f6b00f885a74dbf1cc06968597c810e030110
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-04-04 09:42:06 +0900

    Update ManiActivity.kt

--- Kotin문법 종합반 4주차/널 세이프티/널 인지 아닌지 확인/ManiActivity.kt
+++ Kotin문법 종합반 4주차/널 세이프티/널 인지 아닌지 확인/ManiActivity.kt
@@ -16,11 +16,14 @@ class Student {
     fun displayInfo() {
         println("이름은: ${name} 입니다")
         println("주소는: ${address} 입니다")
+        //address에 서울을 저장하고 displayInfo하면 주소는 서울 입니다 까지 출력
     }
 
     fun displayAddressLength() {
         println("주소의 길이는: ${address?.length} 입니다")
-        //displayAddressLength()는 address길이 호출하는 메소드
-        //address가
+        //참새까지 넣은 단계에 address를 넣지 않았기 때문에 "주소의 길이는: null 입니다"라고 출력
+        //null 발생시에는 프로그램이 종료되지 않고 null이 아니면 호출되고 null이면 null입니라 라고 안전하게 호출
+        //address?.length로 address길이 호출
+        //address가 null인지 보고 null이면 호출하지 않고 null이 아니면 ddress?.length으로 하여 length길이 호출 
     }
 }
