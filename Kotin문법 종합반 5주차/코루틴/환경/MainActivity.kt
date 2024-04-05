c34a319a407b7a2377e7f76e47f54ac13c983547
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-04-06 08:40:46 +0900

    Update and rename MainActivity.kt to MainActivity.kt

--- Kotin문법 종합반 5주차/코루틴/환경/MainActivity.kt
+++ Kotin문법 종합반 5주차/코루틴/환경/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.app
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>) {
     println("메인쓰레드 시작")
@@ -12,9 +11,6 @@ fun main(args: Array<String>) {
         delay(3000)
         println("여기는 코루틴...")
     }//백그라운드에서 비동기적으로 수행되는 것
-    runBlocking {
-        job.join()
-    }//안드로이드 앱같은 경우 메인이 한번 실행되면 앱이 화면에 계속 떠 있고 앱은 죽지 않음
+    //안드로이드 앱같은 경우 메인이 한번 실행되면 앱이 화면에 계속 떠 있고 앱은 죽지 않음
     println("메인쓰레드 종료")
-    //안드로이드에서 이 코드를 똑같이 실행하면 똑같이 나옴
-}
+}//안드로이드에서 이 코드를 똑같이 실행하면 똑같이 나옴
