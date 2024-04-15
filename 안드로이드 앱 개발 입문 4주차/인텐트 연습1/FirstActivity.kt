1279438da21b5f379e306ada472facd90be0e077
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-04-16 06:21:13 +0900

    Update FirstActivity.kt

--- 안드로이드 앱 개발 입문 4주차/인텐트 연습1/FirstActivity.kt
+++ 안드로이드 앱 개발 입문 4주차/인텐트 연습1/FirstActivity.kt
@@ -1,12 +1,24 @@
+package com.example.app
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
 
-        val btn = findViewById<Button>(R.id.buttonFirstActivity)
-        btn.setOnClickListener{
+        val btn = findViewById<Button>(R.id.bt1)
+        btn.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
+        
         }
     }
+
 }
