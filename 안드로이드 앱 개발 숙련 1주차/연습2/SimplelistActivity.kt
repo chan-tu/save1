99ccc12654ec5da255337cbdae72acdcbedb7e1f
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-04-17 15:13:10 +0900

    Update SimplelistActivity.kt

--- 안드로이드 앱 개발 숙련 1주차/연습2/SimplelistActivity.kt
+++ 안드로이드 앱 개발 숙련 1주차/연습2/SimplelistActivity.kt
@@ -13,14 +13,20 @@ class SimplelistActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_simplelist)
 
-        binding= ActivitySimplelistBinding.inflate(layoutInflater)
+        binding = ActivitySimplelistBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val item= arrayOf<String?>("item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item5", "item6", "item7", "item8", "item5", "item6", "item7", "item8", "item5", "item6",  "item7", "item8")
+        //데이터 원본 준비
+        val item = arrayOf<String?>("item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item5", "item6", "item7", "item8", "item5", "item6", "item7", "item8", "item5", "item6", "item7", "item8")
+        //아이템 데이터를 String로 정렬하여 아이템1~8까지 세로로 출력되도록 설정
 
-        val adap=ArrayAdapter(this,android.R.layout.simple_gallery_item,item)
+        //어댑터 준비 (배열 객체 이용, simple_list_item_1 리소스 사용
+        val adap = ArrayAdapter(this, android.R.layout.simple_list_item_1 ,item)
+        //adap를 생성하여 ArrayAdapter를 저장
+        //ArrayAdapter에 android.R.layout.simple_list_item_1과 'item'를 저장
+        //simple_list_item_1는 안드로이드에서 제공하는 레이아웃
 
-        binding.listView.adapter=adap
+        binding.listView.adapter = adap
 
     }
 }
