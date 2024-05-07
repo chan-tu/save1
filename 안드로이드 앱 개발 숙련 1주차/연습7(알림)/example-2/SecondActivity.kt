9e15d6d43ed5ff7d537f0e2729d4bbbef9c3b122
chan-tu <62597830+chan-tu@users.noreply.github.com>
2024-05-07 10:12:22 +0900

    Create SecondActivity.kt

---
+++ 안드로이드 앱 개발 숙련 1주차/연습7(알림)/example-2/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.apppractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.apppractice.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
